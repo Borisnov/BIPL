a562103a3c8b032ed3e8fd224b6abec3a84a573f
Boris <boris2000n@gmail.com>
2018-12-03 18:05:14 +0300

    add begin float integer name states

--- state_machine.cpp
+++ state_machine.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 vector<pair<string, string>>result_tokens;
 string curr_token = "";
+set<string>all_tokens;
+map<string, string>token_type;
 
-void add(string type, string value){
-    result_tokens.push_back(make_pair(type, value));
+void add(string type){
+    result_tokens.push_back(make_pair(type, curr_token));
     curr_token = "";
+}
+
+bool find_token(string token){
+    return all_tokens.count(token);
 }
 
 int main(){
@@ -27,29 +33,56 @@ int main(){
     string state = "begin";
 
     ///Loop of whole code string
-    while(char_index < code_length){
-        c = code[char_index];
+    try{
+        while(char_index < code_length){
+            c = code[char_index];
 
-        if(state == "begin"){
-            if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_')
-                state = "name";
-            else if(c >= '0' && c <= '9')
-                state = "number"
+            if(state == "begin"){
+                if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_')
+                    state = "name";
+                else if(c >= '0' && c <= '9')
+                    state = "number";
 
-        }else if(state == "number"){
-
-        }else if(state == "name"){
-            if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' ||
-            c >= '0' || c <= '9' || c == '_'){
-                curr_token += c;
-                char_index ++;
-            }
-            else{
-                state = "begin";
-                add(curr_token);
+            }else if(state == "integer"){
+                if(c >= '0' || c <= '9'){
+                    curr_token += c;
+                    char_index ++;
+                }else if(c == '.'){
+                    char_index ++;
+                    curr_token += c;
+                    state = "float";
+                }else{
+                    state = "begin";
+                    add("integer");
+                }
+            }else if(state == "float"){
+                if(c >= '0' || c <= '9'){
+                    curr_token += c;
+                    char_index ++;
+                }else{
+                    if(code[char_index - 1] == '.')
+                        throw "Wrong float record";
+                }
+            }else if(state == "name"){
+                if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' ||
+                c >= '0' || c <= '9' || c == '_'){
+                    curr_token += c;
+                    char_index ++;
+                }
+                else{
+                    state = "begin";
+                    string type = "name";
+                    if(find_token(curr_token))
+                        type = token_type[curr_token];
+                    add(type);
+                }
             }
         }
+    }catch(string error){
+        cout<<"Error position = "<<char_index<<'\n';
+        cout<<error;
     }
+
 
 
     return 0;
