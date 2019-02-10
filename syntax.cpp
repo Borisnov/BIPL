f7c26b63a222d4cf85145e3d5ef500e1554bf7fa
Boris <boris2000n@gmail.com>
2019-02-10 20:03:54 +0300

    almost simantic analisys done. Without expressiong

--- syntax.cpp
+++ syntax.cpp
@@ -1,5 +1,6 @@
 #include "bits/stdc++.h"
 #include "state_machine.h"
+#include "../../../../Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/include/utility"
 
 #define DB(x) cout<<(x)<<"\n";
 #define DBE(x) cout<<"Error: "<<(x)<<"\n";
@@ -22,14 +23,85 @@ void block();
 void one_action();
 //bool try_start(void (*f)());
 
+///build poliz
+struct poliz_unit{
+    string status;
+    string type;
+    string value;
+};
+vector<pair<string, string>>poliz;
+
+void poliz_add(string status, string type, string value){
+    poliz_unit action;
+    action.status = status;
+    action.type = type;
+    action.value = value;
+}
+
+void poliz_jump_here(int index){
+    poliz[index].value = to_string(poliz.size());
+}
+
+///semantic functions
+
+string curr_function_return_value = "void";
+vector<map<string, string>> variables (1);  ///find index of var by its name;
+map<string, string>var_type; /// find type of var by its index;
+map<string, vector<string>>functions_types;
+
+void add_function(string name, string type){
+    vector<string>t;
+    t.push_back(type);
+    if(functions_types.find(name) != functions_types.end()){
+        throw "Function with this name already exists";
+    }
+    functions_types[name] = t;
+}
+
+void add_empty_visibility_area(){
+    map<string, string>a;
+    variables.push_back(a);
+}
+
+void delete_last_visabiliy_area(){
+    variables.back().clear();
+    variables.pop_back();
+}
+
+vector<string>parents = {"main"};
+//bool visibility_area;
+int variable_index = 0;
+string new_var_index(){
+    string index = "var_" + to_string(variable_index);
+    variable_index ++;
+}
+
+void add_variable(string name, string type){
+    if(variables.back().find(name) != variables.back().end()){
+        throw "Re initialization of variable";
+    }
+    string new_index = new_var_index();
+    variables.back()[name] = new_index;
+    var_type[new_index] = type;
+}
+
+string get_var_index(string name){
+    for(int i = variables.size() - 1; i >= 0; i--){
+        if(variables[i].find(name) != variables[i].end()){
+            return variables[i][name];
+        }
+    }
+    throw "Unknown variable was found";
+}
+
+///syntax functions
+
 void next(){
-//    cout<<"next called\n";
     if(it == end_it){
         throw "Unexpected end of file";
     }
     ++it;
     if(it == end_it){
-//        throw "End of file";
         type = "END_OF_FILE";
         value = "END_OF_FILE";
         return;
@@ -45,22 +117,57 @@ void check_current(string need_value){
     }
     next();
 }
-int curr_token_type(){
-    if(type == TEXT || type == INT || type == NAME)
-        return 1;
-    if(type == OPERATION)
-        return 0;
-    return 2;
+pair<string, string> token_accurate_type(){
+    if(type == TEXT)
 }
 //expression check
-void expression() {
-    bool b = true;
-    if(curr_token_type() != 1)
-        throw "Wrong expression";
-    while(curr_token_type() <= 1) {
-        if(b && curr_token_type() != 1)
+string expression() {
+    bool begin = true;
+    vector<string>stack;
+    int expressiong_begin = poliz.size();
+    while(true) {
+        pair<string, string>curr_token = token_accurate_type();
+
+        if(begin){
+            if(curr_token.first == "value"){
+                if(curr_token.second == "var") {
+                    check_item(value);
+                }else if(curr_token.second == "value"){
+                    poliz_add("value", type, value);
+                }else if(curr_token.second == "func"){
+
+                }
+            }else if(value == "-"){
+                stack.push_back("b-");
+            }else if(value == "("){
+                stack.push_back(value);
+            }else{
+                throw "Wrong expression";
+            }
+            begin = !begin;
+            next();
+        }else{
+            if(curr_token.first == "post") {
+                string last_var_type = check_poliz(0).type;
+                if(){
+                    poliz_add("execute" , "" , value);
+                }else
+                    throw "Incorrect use of postfix operations";
+            }else if(value == ")"){
+                while(true){
+                    if(stack.size() == 0)
+                        throw "The balance of brackets is not observed";
+                    if(stack.back() == "("){
+                        stack.pop_back();
+                        break;
+                    }
+                    poliz_add("execute", "", "")
+                }
+            }
+        }
+        if(b && token_accurate_type() != 1)
             throw "Wrong expression constant";
-        else if(!b && curr_token_type() != 0)
+        else if(!b && token_accurate_type() != 0)
             throw "Wrong expression operator";
         next();
         b = !b;
@@ -69,7 +176,7 @@ void expression() {
         throw "Wrong end of expression";
 }
 
-void structcheck() {
+void struct_check() {
     check_current("struct");
     if(type != NAME) throw "Skipped name of struct";
     next();
@@ -80,15 +187,27 @@ void structcheck() {
 }
 
 
-void ifcheck() {
+void if_check() {
     check_current("if");
     check_current("(");
-    expression();
+    string exp_type = expression();
+    if(exp_type != "bool")
+        throw "Is statement must have bool type";
     check_current(")");
+    int skip_if_body_pointer = poliz.size();
+    poliz_add("value", "int", "");
+    poliz_add("execute", "", "transition_lie");
+    parents.push_back("if");
     if( value == "{")
         block();
     else
         one_action();
+
+    int skip_else_statement = poliz.size();
+    poliz_add("value", "int", "");
+    poliz_add("execute", "", "transition_abs");
+
+    poliz_jump_here(skip_if_body_pointer);
 
     if(value == "else"){
         next();
@@ -97,36 +216,59 @@ void ifcheck() {
         else
             one_action();
     }
+    poliz_jump_here(skip_else_statement);
+    parents.pop_back();
 }
-//void check_type(string need_type){
-//    if(type != need_type)
-//        throw "Unexpected token type";
-//    next();
-//}
-//for
-void forcheck() {
+
+void for_check() {
+    add_empty_visibility_area();
     check_current("for");
     check_current("(");
+    new_variable();
+    check_current(";");
+    int skip_action_expression=poliz.size();
+    poliz_add("value", "int", "");
+    poliz_add("execute", "", "transition_abs");
+
+    int loop_begin_point = poliz.size();
     expression();
     check_current(";");
-    expression();
-    check_current(";");
-    expression();
+    poliz_jump_here(skip_action_expression);
+
+    string exp_type = expression();
+    if(exp_type != "bool"){
+        throw "<FOR> last condition must be bool";
+    }
+    check_current(")");
+    int skip_loop_block = poliz.size();
+    poliz_add("value", "int", "");
+    poliz_add("execute", "", "transition_lie");
     block();
+    poliz_add("value", "int", to_string(loop_begin_point));
+    poliz_add("execute", "", "transition_abs");
+    poliz_jump_here(skip_loop_block);
 }
 
-//while
-void whilecheck() {
+void while_check() {
     check_current("while");
     check_current("(");
-    expression();
+    string exp_type = expression();
+    if(exp_type != "bool"){
+        throw "<While> condition must be bool";
+    }
     check_current(")");
+    int skip_while_block = poliz.size();
+    poliz_add("value", "int", "");
+    poliz_add("execute", "", "transition_lie");
     block();
+
+    poliz_jump_here(skip_while_block);
 }
 
-void function_parameters() {
+void function_parameters(string func_name) {
     while(true){
         if(type != TYPE) throw "Wrong function parameter";
+        functions_types[func_name].push_back(value);
         next();
         if(type != NAME) throw "Wrong function parameter name";
         next();
@@ -138,28 +280,60 @@ void function_parameters() {
     }
 }
 
-void funccheck() {
+void func_check() {
+    parents.push_back("func");
+    add_empty_visibility_area();
+
     if(type != TYPE) throw "Wrong function declaration";
+
+    curr_function_return_value = value;
     next();
+
     if(type != NAME) throw "Wrong function name";
+    string func_name = value;
+    add_function(value, curr_function_return_value);
     next();
+
     check_current("(");
-    function_parameters();
+    function_parameters(func_name);
     check_current(")");
+
     block();
+    curr_function_return_value = "void";
+    delete_last_visabiliy_area();
 }
+
 void expression_action(){
     expression();
     check_current(";");
 }
+
 void one_action(){
-    if(value == "if") ifcheck();
-    else if(value == "for") forcheck();
-    else if(value == "while") whilecheck();
+    if(value == "if") if_check();
+    else if(value == "for") for_check();
+    else if(value == "while") while_check();
+    else if(value == "break"){
+        string parent = parents.back();
+        if(!(parent == "while" || parent == "for")){
+            throw "Break can be only in while or for loop";
+        }
+        check_current(";");
+    }
+    else if(value == "continue"){
+        string parent = parents.back();
+        if(!(parent == "while" || parent == "for")){
+            throw "Break can be only in while or for loop";
+        }
+        check_current(";");
+    }
+    else if(value == "return"){
+        if(curr_function_return_value == "void"){
+        }else{
+            string exp_type = expression();
+        }
+        check_current(";");
+    }
     else if(type == TYPE) new_variable();
-    else if(value == "break"){}
-    else if(value == "continue"){}
-    else if(value == "return"){}
     else expression_action();
 }
 
@@ -173,13 +347,18 @@ void block() {
 
 void new_variable() {
     if(type != TYPE) throw "Skipped type of variable";
+    string var_type = type;
     next();
     while (true) {
         if(type != NAME) throw "Expected variable name";
+        string var_name = value;
         next();
         if(value == "="){
             next();
-            expression();
+            string val_type = expression();
+            if(val_type != var_type)
+                throw "Value of variable must be the same type as variable";
+            add_item(var_name, )
         }
         if(value == ";"){
             next();
@@ -190,23 +369,14 @@ void new_variable() {
     }
 }
 
-//bool try_start(void (*f)()){
-//    try{
-//        f();
-//        return true;
-//    }catch(...){
-//        return false;
-//    }
-//}
-
 void global_block(){
     block();
 }
 
 void program_body() {
     while (it < end_it) {
-        if (value == STRUCT) structcheck();
-        else if (type == TYPE) funccheck();
+        if (value == STRUCT) struct_check();
+        else if (type == TYPE) func_check();
         else if (value == "{") global_block();
         else throw ("Unexpected token " + value).c_str();
     }
@@ -258,7 +428,7 @@ int main() {
         cout << error<<'\n';
         return 0;
     }
-    cout<<"SUCCESS COMPILING!";
+    cout<<"SUCCESS COMPILING!" << '\n';
 
 
 }
